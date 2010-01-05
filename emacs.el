95bb1f7c3e6a57c906501236f3fe2e79abd8d90c
Tom Small <tsmall3@gmail.com>
2010-01-04 21:47:21 -0500

    Change back to Monaco 10pt anti-aliased

--- emacs.el
+++ emacs.el
@@ -5,6 +5,13 @@
   ;; If there is more than one, they won't work right.
  '(tool-bar-mode nil))
 
+(custom-set-faces
+  ;; custom-set-faces was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ '(default ((t (:stipple nil :background "white" :foreground "black" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 100 :width normal :family "apple-monaco")))))
+
 ;; -----------------------------------------------------------------------------
 
 ;; General customizations
@@ -21,7 +28,7 @@
 ;; Turn off anti-aliasing
 ;; Note: To actually not use anti-aliasing I had to also run this command:
 ;;   defaults write org.gnu.Emacs AppleAntiAliasingThreshold 100
-;(setq mac-allow-anti-aliasing nil)
+(setq mac-allow-anti-aliasing nil)
 
 (add-to-list 'load-path "~/.emacs.d/site-lisp")
 
