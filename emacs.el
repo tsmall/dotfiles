348709e529ccc59992ad786e44b24aa524119ffe
Tom Small <tsmall3@gmail.com>
2010-01-09 13:00:51 -0500

    Separate local emacs settings from common ones
    The emacs.el script (and emacs.d directory) only contains settings
    that apply to all of my computers now. All of the local settings are
    stored in the .emacs file in my home directory, which loads the common
    settings.

--- emacs.el
+++ emacs.el
@@ -1,20 +1,6 @@
-(custom-set-variables
-  ;; custom-set-variables was added by Custom.
-  ;; If you edit it by hand, you could mess it up, so be careful.
-  ;; Your init file should contain only one such instance.
-  ;; If there is more than one, they won't work right.
- '(tool-bar-mode nil))
-
-(custom-set-faces
-  ;; custom-set-faces was added by Custom.
-  ;; If you edit it by hand, you could mess it up, so be careful.
-  ;; Your init file should contain only one such instance.
-  ;; If there is more than one, they won't work right.
- '(default ((t (:stipple nil :background "white" :foreground "black" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 100 :width normal :family "apple-monaco")))))
-
 ;; -----------------------------------------------------------------------------
-
 ;; General customizations
+
 (setq transient-mark-mode t) ;; Highlight regions
 (setq line-number-mode t) ;; Display line numbers
 (setq column-number-mode t) ;; Display column numbers
@@ -25,10 +11,7 @@
 (setq visible-bell t) ;; Disable beep
 (setq confirm-kill-emacs 'yes-or-no-p) ; Confirm quit
 
-;; Turn off anti-aliasing
-;; Note: To actually not use anti-aliasing I had to also run this command:
-;;   defaults write org.gnu.Emacs AppleAntiAliasingThreshold 100
-(setq mac-allow-anti-aliasing nil)
+(add-to-list 'load-path "~/.emacs.d/site-lisp")
 
 (add-to-list 'load-path "~/.emacs.d/site-lisp")
 
@@ -70,3 +53,4 @@
 ;; JavaScript
 (autoload 'js2-mode "js2" nil t)
 (add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
+
