9354596587000f84c6f0c6ca5d7c278426d1f2bf
Tom Small <tsmall3@gmail.com>
2011-06-04 17:07:55 -0400

    emacs: add caml-mode

--- emacs.el
+++ emacs.el
@@ -69,3 +69,11 @@
 ;; Markdown
 (autoload 'markdown-mode "markdown-mode.el"
   "Major mode for editing Markdown files" t)
+
+;; OCaml
+(setq auto-mode-alist
+          (cons '("\\.ml[iylp]?$" . caml-mode) auto-mode-alist))
+(autoload 'caml-mode "caml" "Major mode for editing Caml code." t)
+(autoload 'run-caml "inf-caml" "Run an inferior Caml process." t)
+(if window-system (require 'caml-font))
+
