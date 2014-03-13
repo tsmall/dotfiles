8f516d06b87754b418a4ba3a800f6544a4e939ed
Tom Small <tom@offerpop.com>
2014-03-13 19:11:10 -0400

    conkeror: update dev webjumps

--- conkeror/trs-webjumps.js
+++ conkeror/trs-webjumps.js
@@ -3,10 +3,12 @@ define_webjump("timeswire", "http://www.nytimes.com/timeswire");
 
 // APIs
 
+define_webjump("fbjs", "https://developers.facebook.com/docs/reference/javascript/");
 define_webjump("graphapi", "https://developers.facebook.com/docs/reference/api/");
 
 // Databases
 
+define_webjump("mongodb", "http://docs.mongodb.org/manual/");
 define_webjump("mysql", "http://dev.mysql.com/doc/refman/5.1/en/index.html");
 
 // Haskell
@@ -16,7 +18,7 @@ define_webjump("hoogle", "http://www.haskell.org/hoogle/?hoogle=%s",
 
 // HTML and CSS
 
-define_webjump("bootstrap", "http://twitter.github.com/bootstrap/");
+define_webjump("bootstrap", "http://getbootstrap.com");
 define_webjump("css", "https://developer.mozilla.org/en/CSS/CSS_Reference");
 define_webjump("dom", "https://developer.mozilla.org/en/Gecko_DOM_Reference");
 define_webjump("html", "https://developer.mozilla.org/en/HTML");
@@ -24,18 +26,26 @@ define_webjump("html", "https://developer.mozilla.org/en/HTML");
 // JavaScript
 
 define_webjump("backbone", "http://documentcloud.github.com/backbone/");
-define_webjump("jqapi", "http://jqapi.com");
+define_webjump("jasmine", "http://pivotal.github.io/jasmine/");
+define_webjump("jquery", "http://api.jquery.com");
 define_webjump("js", "https://developer.mozilla.org/en/JavaScript/Reference");
 define_webjump("requirejs", "http://requirejs.org/docs/api.html");
 define_webjump("underscore", "http://documentcloud.github.com/underscore/");
 
 // Python
 
+define_webjump("celery", "http://docs.celeryproject.org/en/latest/index.html");
 define_webjump("django", "https://docs.djangoproject.com/en/");
+define_webjump("flask", "http://flask.pocoo.org/docs");
 define_webjump("haystack", "http://django-haystack.readthedocs.org/en/latest/index.html");
+define_webjump("pymock", "http://www.voidspace.org.uk/python/mock/");
 define_webjump("python", "http://docs.python.org/2/");
 define_webjump("python2.5", "http://docs.python.org/release/2.5.4/");
 define_webjump("sqlalchemy", "http://docs.sqlalchemy.org/en/rel_0_5/");
+
+// REST
+
+define_webjump("siren", "https://github.com/kevinswiber/siren");
 
 // Tools
 
