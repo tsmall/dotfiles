f6378d9d258573dae0d4cd19afec4a09db1619ed
Tom Small <tom@offerpop.com>
2014-09-12 11:46:37 -0400

    emacs: update fiplr's ignore settings

--- emacs.d/el-get-init-files/init-fiplr.el
+++ emacs.d/el-get-init-files/init-fiplr.el
@@ -1,1 +1,4 @@
 (global-set-key (kbd "C-c t") 'fiplr-find-file)
+
+(setq fiplr-ignored-globs '((directories (".git" ".svn" ".hg" ".bzr" "bower_components" "node_modules"))
+                            (files (".#*" "*~" "*.so" "*.jpg" "*.png" "*.gif" "*.gz" "*.pdf" "*.pyc" "*.zip"))))
