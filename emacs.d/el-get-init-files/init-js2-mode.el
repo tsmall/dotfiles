d6481fcd10f08e8ee2d58373220d253c1871c684
Tom Small <tsmall@schoology.com>
2018-01-19 18:46:01 -0500

    emacs: update js2-mode settings

--- emacs.d/el-get-init-files/init-js2-mode.el
+++ emacs.d/el-get-init-files/init-js2-mode.el
@@ -1,2 +1,8 @@
 (add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
+
 (setq js2-indent-switch-body t)
+
+;; Customize js2-mode's lint warnings.
+(setq js2-strict-missing-semi-warning nil)
+(setq js2-missing-semi-one-line-override nil)
+(setq js2-strict-trailing-comma-warning nil)
